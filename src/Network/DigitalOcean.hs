c33fd0116a24464f2aa6c53251d9a8968b07aa33
Yiğit Özkavcı <yigitozkavci8@gmail.com>
2017-07-15 01:45:03 +0300

    Make use of DO monad

--- src/Network/DigitalOcean.hs
+++ src/Network/DigitalOcean.hs
@@ -4,7 +4,6 @@
 module Network.DigitalOcean where
 
 ------------------------------------------------
-
 import           Control.Monad
 import           Control.Monad.Reader
 import           Control.Monad.Except
@@ -14,7 +13,6 @@ import           Network.HTTP.Client.TLS
 import qualified Data.ByteString         as BS
 import qualified Data.ByteString.Char8   as BSC
 import           System.Environment
-
 ------------------------------------------------
 
 data Client = Client { apiKey :: BS.ByteString }
@@ -24,14 +22,17 @@ type DoErr = T.Text
 newtype DO a = DO { runDO :: ReaderT Client (ExceptT String IO) a }
   deriving (Functor, Applicative, Monad, MonadIO, MonadError String, MonadReader Client)
 
-get :: IO ()
+get :: DO ()
 get = do
-  manager <- newTlsManager
-  initialRequest <- parseRequest "https://api.digitalocean.com/v2/account" 
-  client <- getClient
+  manager <- liftIO $ newTlsManager
+  initialRequest <- liftIO $ parseRequest "https://api.digitalocean.com/v2/account" 
+  client <- liftIO getClient
   let request = initialRequest { method = "GET", requestHeaders = [("Authorization", "Bearer " `BS.append` apiKey client)] }
-  response <- httpLbs request manager
-  print response
+  response <- liftIO $ httpLbs request manager
+  liftIO $ print response
+
+runReq :: Client -> DO a -> IO (Either String a)
+runReq client do' = runExceptT $ runReaderT (runDO do') client
 
 getClient :: IO Client
 getClient = Client . BSC.pack <$> getEnv "DO_TOKEN"
